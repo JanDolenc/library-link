a40e15654cc611aa05c9198802960c051ffb5799
Jan Dolenc <jan.dolenc9@gmail.com>
2024-10-23 14:21:44 +0200

    feat: create server & handle root route

--- main.go
+++ main.go
@@ -1,7 +1,23 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+)
+
 
 func main() {
-	fmt.Println("Welcome to Library Link.")
+	router := http.NewServeMux()
+
+	router.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("Welcome Library Link user. See /docs for more information."))
+	})
+
+	server := http.Server{
+		Addr:    ":8081",
+		Handler: router,
+	}
+
+	fmt.Println("Starting server on port :8081")
+	server.ListenAndServe()
 }

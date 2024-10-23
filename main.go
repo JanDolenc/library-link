8559af99a6ce29ef49b1c6df2b1fa503f267e535
Jan Dolenc <jan.dolenc9@gmail.com>
2024-10-23 19:39:56 +0200

    refactor: add handler function for root path

--- main.go
+++ main.go
@@ -2,16 +2,20 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
 )
+
+func greet(w http.ResponseWriter, r *http.Request) {
+	log.Println("Received a request at path /")
+	w.Write([]byte("Welcome Library Link user. See /docs for more information."))
+}
 
 
 func main() {
 	router := http.NewServeMux()
 
-	router.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("Welcome Library Link user. See /docs for more information."))
-	})
+	router.HandleFunc("GET /", greet)
 
 	server := http.Server{
 		Addr:    ":8081",
